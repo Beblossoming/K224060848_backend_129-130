ebaf58ddb20c671657a7c0fefcfb086db6c751f7
Beblossoming <118942026+Beblossoming@users.noreply.github.com>
2024-10-30 22:46:54 +0700

    yes

--- my-server-mongodb/Index.js
+++ my-server-mongodb/Index.js
@@ -3,10 +3,13 @@ const app = express();
 const port = 3002;
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
 const cors = require('cors');
 const { MongoClient, ObjectId } = require('mongodb');
 const multer = require('multer');
 
+app.use(express.json());
 // Cấu hình morgan để log request
 app.use(morgan('combined'));
 
@@ -23,7 +26,7 @@ app.use(cors(corsOptions));
 
 // MongoDB kết nối
 const client = new MongoClient('mongodb://127.0.0.1:27017', { useNewUrlParser: true, useUnifiedTopology: true });
-let fashionCollection;
+let fashionCollection, userCollection;
 
 // Cấu hình multer cho file upload
 const storage = multer.memoryStorage();
@@ -35,6 +38,7 @@ client.connect()
 
     const database = client.db('FashionData');
     fashionCollection = database.collection('Fashion');
+    userCollection = database.collection('User');
 
     // Route mặc định
     app.get("/", (req, res) => {
@@ -85,13 +89,49 @@ client.connect()
         res.status(500).send('Error saving fashion data');
       }
     });
+    
+    app.get("/auth/read-cookie", (req, res) => {
+      const username = req.cookies.username || 'Guest'; // Lấy username từ cookie
+      res.send({ username }); // Trả về username
+    });
+    
+    app.get("/auth/clear-cookie", (req, res) => {
+      res.clearCookie("username");
+      res.clearCookie("password");
+      res.send("Cookies for username and password have been removed");
+    });
 
+    app.post("/auth/login", async (req, res) => {
+      const { username, password } = req.body; // Thay đổi từ name thành username
+    
+      console.log('Login attempt with:', { username, password });
+    
+      try {
+        const user = await userCollection.findOne({ username: username }); // Tìm người dùng theo username
+    
+        if (user) {
+          // So sánh mật khẩu
+          if (user.password === password) {
+            res.cookie("username", user.username, { httpOnly: true, maxAge: 86400000 });
+            res.send({ message: 'Login successful', user });
+          } else {
+            res.status(401).send('Invalid username or password'); // Sai mật khẩu
+          }
+        } else {
+          res.status(401).send('Invalid username or password'); // Không tìm thấy người dùng
+        }
+      } catch (error) {
+        console.error('Login error:', error);
+        res.status(500).send('Error logging in');
+      }
+    });
+    
   })
   .catch((error) => {
     console.error('MongoDB connection error:', error);
   });
 
-// Lắng nghe trên cổng 3002
+// Lắng nghe trên cổng 3000
 app.listen(port, () => {
   console.log(`My Server listening on port ${port}`);
 });
